f8c90145ad577ccb8bb0fe2e7f5467c2a84d98d1
Tiia Laukkanen <tlaukkan@student.hive.fi>
2020-04-14 15:57:12 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var gamePattern = [];
 var userClickedPattern = [];
 var started = false;
 var level = 0;
-alert("At the beginning of the game, one of the 4 keys lights up randomly producing simultaneously a sound associated to the key. The player has to press the same key. Next, the Simon turns back the same light on and a second one, again randomly. The player has to reproduce this chain of light using his memory. And so on... In each round a new key is added to the series and the game becomes all the more difficult as the player's memory is put to the test. If the player doesn't make any mistake, the game goes on, so it is an endless game!");
 
 $(document).keydown(function() {
 	if (!started) {
